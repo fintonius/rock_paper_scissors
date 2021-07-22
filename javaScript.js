15aea3676a2eda46828102ea5fdbd1b4d986a378
fintonius <fintonius.mcginty@gmail.com>
2021-07-22 21:15:32 +0100

    still can't get score to be kept and new round to be called javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -2,18 +2,21 @@
    let choices = [`rock`, `paper`, `scissors`];  
    let compScore = 0;
     let playerScore = 0;
+    let gamesPlayed = 0;
+    let playerAnswer = prompt(`Please enter one of the following: 'rock', paper' or 'scissors' `);
 
    function computerPlay() {               
        let selection = choices[Math.floor(Math.random() * choices.length)];
        return selection;
    }
 
-   let playerAnswer = prompt(`Please enter one of the following: 'rock', paper' or 'scissors' `)
 
-
-   function playRound(playerSelection, computerSelection) {
-
+   function game(playerSelection, computerSelection) {
+     if (gamesPlayed < 4) {
+        alert(playerAnswer = prompt(`Please enter one of the following: 'rock', paper' or 'scissors' `));
+    }
     if (playerSelection == computerSelection) {
+        gamesPlayed++;
         return(`It's a draw`);
     } else if (
      (computerSelection == `rock` && playerSelection == `scissors`) ||
@@ -22,16 +25,21 @@
      )
      {
          compScore++;
+         gamesPlayed++;
          return(`you lose!`);
      } else {
          playerScore++;
+         gamesPlayed++;
          return(`you win!`);
-     }
-            
+     }                 
+ } 
+ 
+ function keepPlaying() {
+     if (gamesPlayed < 4) {
+    alert(game(playerSelection, computerSelection));
+}
  }
-
-
-
+ 
     //stage 1 complete! now need to make a 5 round game :(
     //can use a 'for loop' I think. Pseudo-code first though! 
     //round 1 prompts player to enter answer. Compares against compter answer to determine winner
@@ -44,12 +52,14 @@
 
     //have a variable for the comp & player scores to store the increments in?
     
-    function game() {    
-
-    }
+    
 
    let playerSelection = playerAnswer.toLowerCase(); 
    let computerSelection = computerPlay();
    console.log(playerSelection, computerSelection);
-   alert(playRound(playerSelection, computerSelection));
+   alert(game(playerSelection, computerSelection));
+   alert(game(playerSelection, computerSelection));
+   alert(game(playerSelection, computerSelection));
    console.log(compScore, playerScore);
+   console.log(gamesPlayed);
+   
